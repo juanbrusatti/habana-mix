4e8da8d1705e9a43d52a8623795a5641fd3dac0c
Juan Brusatti <mjuanmolina123@gmail.com>
2026-08-21 18:07:54 -0300

    change favicon

--- habana-mix./app/layout.tsx
+++ habana-mix./app/layout.tsx
@@ -37,21 +37,8 @@ export const metadata: Metadata = {
     type: 'website',
   },
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: '/images/icono.jpeg',
+    apple: '/images/icono.jpeg',
   },
 }
 
